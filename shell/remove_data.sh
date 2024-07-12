2ca256fb3f77cd9f2e54249dc6189f11040322b6
GeonHyeock <gunheock@gmail.com>
2024-07-12 16:31:49 +0900

    Add : data remove sh

---
+++ shell/remove_data.sh
@@ -0,0 +1,7 @@
+rm -rf LightningHydraTemplate/data/raw_data/train
+rm -rf LightningHydraTemplate/data/raw_data/valid
+rm -rf LightningHydraTemplate/data/raw_data/test
+
+rm -rf LightningHydraTemplate/data/raw_data/train_remove.csv
+rm -rf LightningHydraTemplate/data/raw_data/valid_remove.csv
+rm -rf LightningHydraTemplate/data/raw_data/test_remove.csv
