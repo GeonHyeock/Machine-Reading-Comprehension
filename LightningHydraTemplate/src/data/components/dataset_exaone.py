156eb328f198f21063d850d2ee24e6b3de0f4ba9
GeonHyeock <gunheock@gmail.com>
2024-08-08 09:22:47 +0900

    Feat : exaone

---
+++ LightningHydraTemplate/src/data/components/dataset_exaone.py
@@ -0,0 +1,130 @@
+from torch.utils.data import Dataset
+from src.models.roberta_module import normalize_answer
+from tqdm import tqdm
+import pandas as pd
+import torch
+import json
+import os
+
+
+class MyDataset(Dataset):
+    def __init__(self, df, tokenizer, data_folder_path, dtype, fold, max_length=384, stride=128):
+        self.df = df
+        self.tokenizer = tokenizer
+        self.path = os.path.join(data_folder_path, dtype, fold)
+        self.data_folder_path = data_folder_path
+        self.dtype = dtype
+
+        remove_id, use_id = [], []
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
+            idx = 0
+            for _, data in tqdm(self.df.iterrows(), total=len(self.df), desc=f"{self.dtype} data preprocess"):
+                data = {
+                    "id": [data.id],
+                    "context": [data.context],
+                    "question": [data.question],
+                    "answers": ["" if self.dtype == "test" else data.answer],
+                }
+                preprocess_data = self.preprocess_function(data, max_length, stride)
+                for i in range(len(preprocess_data["start_positions"])):
+                    sub_data = {k: v[i].tolist() for k, v in preprocess_data.items()}
+                    sub_data.update(data)
+                    if self.dtype == "test":
+                        with open(os.path.join(self.path, f"{idx}.json"), "w", encoding="utf-8") as f:
+                            json.dump(sub_data, f, indent=4)
+                            idx += 1
+
+                    else:
+                        if sub_data["start_positions"] != sub_data["end_positions"]:
+                            text = sub_data["answers"][0]
+                            decode_text = sub_data["input_ids"][sub_data["start_positions"] : sub_data["end_positions"] + 1]
+                            decode_text = self.tokenizer.decode(decode_text)
+                            if normalize_answer(text) == normalize_answer(decode_text):
+                                with open(os.path.join(self.path, f"{idx}.json"), "w", encoding="utf-8") as f:
+                                    json.dump(sub_data, f, indent=4)
+                                    idx += 1
+                                    use_id += sub_data["id"]
+                        else:
+                            remove_id += sub_data["id"]
+            remove_id = list(set(remove_id) - set(use_id))
+            pd.DataFrame(remove_id).to_csv(os.path.join(data_folder_path, f"{dtype}_{fold}_remove.csv"), index=False)
+
+    def __len__(self):
+        return len(os.listdir(self.path))
+
+    def __getitem__(self, idx):
+        return json.load(open(os.path.join(self.path, f"{idx}.json"), "r"))
+
+    def preprocess_function(self, raw_text, max_length=384, stride=128):
+        inputs = self.tokenizer(
+            f"[|system|]You are EXAONE model from LG AI Research, a helpful assistant.[|endofturn|]\n[|user|] 지문을 읽고 내 질문에 답변해줘! \n 질문 : {raw_text['question'][0].strip()} \n 지문 : ",
+            f"{raw_text['context'][0].strip()} \n[|assistant|]",
+            max_length=max_length,
+            stride=stride,
+            truncation="only_second",
+            return_overflowing_tokens=True,
+            padding="max_length",
+            return_tensors="pt",
+        )
+
+        start_positions, end_positions, context_position = [], [], []
+        for i, input_ids in enumerate(inputs["input_ids"]):
+            sequence_ids = inputs.sequence_ids(i) + [None]
+
+            # Find the start and end of the context
+            idx = 0
+            while sequence_ids[idx] != 1:
+                idx += 1
+            context_start = idx
+            while sequence_ids[idx] == 1:
+                idx += 1
+            context_end = idx - 1
+            context_position += [(context_start, context_end)]
+
+            start, end = self.start_end(raw_text["answers"][0], input_ids, context_start, context_end)
+            start_positions.append(start)
+            end_positions.append(end)
+
+        inputs["start_positions"] = torch.tensor(start_positions)
+        inputs["end_positions"] = torch.tensor(end_positions)
+        inputs["context_position"] = torch.tensor(context_position)
+        return inputs
+
+    def start_end(self, text, input_ids, context_start, context_end):
+        start, end = 0, 0
+        answer = torch.tensor(self.tokenizer(text)["input_ids"])
+        answer_length = len(answer)
+        for idx in range(context_start, context_end - answer_length):
+            if (input_ids[idx : idx + answer_length] == answer).all():
+                start = idx
+                end = idx + answer_length - 1
+        if start * end > 0:
+            return start, end
+
+        answer = torch.tensor(self.tokenizer(" " + text)["input_ids"])
+        answer_length = len(answer)
+        for idx in range(context_start, context_end - answer_length):
+            if (input_ids[idx : idx + answer_length] == answer).all():
+                start = idx
+                end = idx + answer_length - 1
+        return start, end
+
+
+class Collate_fn:
+    def __init__(self, dtype):
+        self.dtype = dtype
+
+    def __call__(self, batch):
+        data = pd.DataFrame(batch)
+        return {
+            "input_ids": torch.tensor(data["input_ids"]),
+            "attention_mask": torch.tensor(data["attention_mask"]),
+            "start_positions": torch.tensor(data["start_positions"]),
+            "end_positions": torch.tensor(data["end_positions"]),
+            "context_position": data["context_position"],
+            "id": data["id"],
+            "context": data["context"],
+            "question": data["question"],
+            "answers": data["answers"],
+        }
---
+++ LightningHydraTemplate/src/models/components/exaone.py
@@ -0,0 +1,109 @@
+import torch
+from torch import nn
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import inject_adapter_in_model, LoraConfig
+
+
+class EXAONE(nn.Module):
+    def __init__(
+        self,
+        name="LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct",
+        lora_module=["attention.q_proj", "attention.k_proj", "attention.v_proj", "attention.out_proj"],
+        QaOutput_Version=1,
+    ) -> None:
+        super(EXAONE, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(name)
+        self.model = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True)
+
+        if lora_module:
+            lora_target = [
+                name
+                for name, module in self.model.named_modules()
+                if ((isinstance(module, nn.Linear) or isinstance(module, nn.Conv1d))) and any([m in name for m in lora_module])
+            ]
+            lora_config = LoraConfig(
+                lora_alpha=8,
+                lora_dropout=0.2,
+                r=16,
+                bias="lora_only",
+                target_modules=lora_target,
+            )
+            self.model = inject_adapter_in_model(lora_config, self.model)
+
+            if QaOutput_Version == 1:
+                self.model.set_output_embeddings(QaOutput(4096))
+            elif QaOutput_Version == 2:
+                self.model.set_output_embeddings(QaOutputV2(4096))
+
+    def forward(self, x) -> torch.Tensor:
+        logits = self.model(**x).logits
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1).contiguous()  # (bs, max_query_len)
+        end_logits = end_logits.squeeze(-1).contiguous()  # (bs, max_query_len)
+        return {"start_logits": start_logits, "end_logits": end_logits}
+
+
+class QaOutput(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.classifier = nn.Sequential(
+            *[
+                nn.Dropout(0.33),
+                nn.Linear(hidden_size, 2, bias=False),
+            ]
+        )
+
+    def forward(self, x):
+        return self.classifier(x)
+
+
+class QaOutputV2(nn.Module):
+    def __init__(self, hidden_size, r=4):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            *[
+                nn.Conv1d(hidden_size, hidden_size // (r // 2), 1, 1),
+                nn.GELU(),
+            ]
+        )
+        self.conv2 = nn.Sequential(
+            *[
+                nn.Conv1d(hidden_size, hidden_size // r, 3, 1, 1),
+                nn.GELU(),
+            ]
+        )
+        self.conv3 = nn.Sequential(
+            *[
+                nn.Conv1d(hidden_size, hidden_size // r, 5, 1, 2),
+                nn.GELU(),
+            ]
+        )
+
+        self.dropout = nn.Dropout(0.33)
+
+        self.classifier = nn.Sequential(
+            *[
+                nn.Dropout(0.5),
+                nn.Linear(hidden_size, 2, bias=False),
+            ]
+        )
+
+    def forward(self, x):
+        x = self.dropout(x.permute(0, 2, 1))
+        x = torch.cat([conv(x) for conv in [self.conv1, self.conv2, self.conv3]], dim=1)
+        x = x.permute(0, 2, 1)
+
+        logits = self.classifier(x)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1).contiguous()
+        end_logits = end_logits.squeeze(-1).contiguous()
+        return {"start_logits": start_logits, "end_logits": end_logits}
+
+
+if __name__ == "__main__":
+    net = lamma()
+    ckpt = torch.load(
+        "/shared/home/ai_math/2024MRC/LightningHydraTemplate/logs/train/runs/512_fold1_lamma/checkpoints/epoch_001.ckpt",
+        map_location="cpu",
+    )
+    print(1)
